97687f05eb8696b9d0cd700f77ec6a24cbe9293e
Vanya_Coffee <vanakupnevskij@gmail.com>
2023-05-25 19:21:34 +0300

    fix get all

--- queries/records/getAll.sql
+++ queries/records/getAll.sql
@@ -5,19 +5,22 @@ SELECT records.id AS id,
        records.money_account AS money_account,
        comment,
        source_from,
+       
        income.price AS income,
+
        costs.price AS costs,
        costs.already_paid AS already_paid,
-    workers.full_name AS worker_full_name,
-    workers.money_account AS worker_money_account,
-    workers.realm AS worker_realm,
-    workers.salary AS worker_salary
+
+       workers.full_name AS worker_full_name,
+       workers.money_account AS worker_money_account,
+       workers.realm AS worker_realm,
+       workers.salary AS worker_salary
     FROM records 
         INNER JOIN income ON
             records.id = income.records_id
         INNER JOIN costs ON
             records.id = costs.records_id
-        INNER JOIN workers ON
+        LEFT JOIN workers ON
             costs.workers_id = workers.id
         WHERE records.owner_id = ?;
 
