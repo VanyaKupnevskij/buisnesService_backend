8a0414e13614d08f537cd1f60a70b4c7d82b2925
Vanya_Coffee <vanakupnevskij@gmail.com>
2023-05-26 23:47:03 +0300

    additionalInfo for records

--- services/RecordService.js
+++ services/RecordService.js
@@ -56,7 +56,7 @@ class RecordService extends BaseService {
   getAll = async ({ start_date, end_date, owner_id }) => {
     const items = await this.repository.getAll(start_date, end_date, owner_id);
 
-    return items;
+    return this.calculateBuisnesValues(items);
   };
 
   getById = async (id) => {
@@ -70,6 +70,63 @@ class RecordService extends BaseService {
     if (!isSeccessful) {
       throw new AppError(ERROR_PRESETS.DELETE_ENTITY_BY_ID(id));
     }
+  };
+
+  calculateBuisnesValues = (records) => {
+    let resultRecords = [...records];
+    const additionalInfo = {
+      year: {},
+      month: {},
+      day: {},
+    };
+
+    for (let record of resultRecords) {
+      const year = new Date(record.date).getFullYear();
+      const month = new Date(record.date).getMonth() + 1;
+      const day = new Date(record.date).getDate();
+
+      additionalInfo.year[year] = {
+        income: (additionalInfo.year[year]?.income || 0) + record.income,
+        costs: (additionalInfo.year[year]?.costs || 0) + record.costs,
+        already_paid: (additionalInfo.year[year]?.already_paid || 0) + record.already_paid,
+      };
+
+      additionalInfo.month[month] = {
+        income: (additionalInfo.month[month]?.income || 0) + record.income,
+        costs: (additionalInfo.month[month]?.costs || 0) + record.costs,
+        already_paid: (additionalInfo.month[month]?.already_paid || 0) + record.already_paid,
+      };
+
+      additionalInfo.day[day] = {
+        income: (additionalInfo.day[day]?.income || 0) + record.income,
+        costs: (additionalInfo.day[day]?.costs || 0) + record.costs,
+        already_paid: (additionalInfo.day[day]?.already_paid || 0) + record.already_paid,
+      };
+
+      for (const [keyYear, infoYear] of Object.entries(additionalInfo.year)) {
+        additionalInfo.year[keyYear].margin = infoYear.income - infoYear.costs;
+        additionalInfo.year[keyYear].marginality =
+          (additionalInfo.year[keyYear].margin / infoYear.income) * 100;
+        additionalInfo.year[keyYear].profitability =
+          (additionalInfo.year[keyYear].margin / infoYear.income) * 100;
+      }
+      for (const [keyMonth, infoMonth] of Object.entries(additionalInfo.month)) {
+        additionalInfo.month[keyMonth].margin = infoMonth.income - infoMonth.costs;
+        additionalInfo.month[keyMonth].marginality =
+          (additionalInfo.month[keyMonth].margin / infoMonth.income) * 100;
+        additionalInfo.month[keyMonth].profitability =
+          (additionalInfo.month[keyMonth].margin / infoMonth.income) * 100;
+      }
+      for (const [keyDay, infoDay] of Object.entries(additionalInfo.day)) {
+        additionalInfo.day[keyDay].margin = infoDay.income - infoDay.costs;
+        additionalInfo.day[keyDay].marginality =
+          (additionalInfo.day[keyDay].margin / infoDay.income) * 100;
+        additionalInfo.day[keyDay].profitability =
+          (additionalInfo.day[keyDay].margin / infoDay.income) * 100;
+      }
+    }
+
+    return { resultRecords, additionalInfo };
   };
 }
 
