c51eda68a536e6fae8c430b7fdbeeb4ab41ee911
Vanya_Coffee <vanakupnevskij@gmail.com>
2023-06-07 18:37:29 +0300

    add client

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ app.use('/api/records/', recordRouter);
 app.use('/api/projects/', projectRouter);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+  app.use('/', express.static(path.resolve('client', 'build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
   });
 }
 
